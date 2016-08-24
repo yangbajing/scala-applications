901bc12a80ea5910f901ee418234bcac2b9c0d11
Yang Jing <yangbajing@gmail.com>
2016-08-24 08:18:06 +0800

    add springscala projects

---
+++ springscala/web/src/main/java/me/yangbajing/demo/SpringscalaApplication.java
@@ -0,0 +1,12 @@
+package me.yangbajing.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringscalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringscalaApplication.class, args);
+	}
+}
---
+++ springscala/web/src/test/java/me/yangbajing/demo/SpringscalaApplicationTests.java
@@ -0,0 +1,16 @@
+package me.yangbajing.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringscalaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
